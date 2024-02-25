c654780da4e2a47cbfbfaeed0d63811cdedc0083
Radek <radekprzewuski@gmail.com>
2024-02-25 14:57:42 +0100

    configuration update

--- vite.config.js
+++ vite.config.js
@@ -6,16 +6,18 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'bridgingTheGap',
-  server: {
-    host: '0.0.0.0'
-  },
+  base: '/bridgingTheGap',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
   preview: {
+    host: '::',
+    port: 5500,
+    base: '/bridgingTheGap'
+  },
+  server: {
     host: '::',
     port: 5500
   }
